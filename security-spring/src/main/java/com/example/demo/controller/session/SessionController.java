a5bfe1cd53df68ee9154952f2a14caf9a9bc5d51
liaojames <liaojames@localhost>
2024-04-10 14:46:07 +0800

    security

--- security-spring/src/main/java/com/example/demo/controller/session/SessionController.java
+++ security-spring/src/main/java/com/example/demo/controller/session/SessionController.java
@@ -1,17 +1,18 @@
 package com.example.demo.controller.session;
+import javax.servlet.http.Cookie; 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
+import java.util.UUID;
 @RestController
 public class SessionController {
+	
 	@Autowired
 	HttpSession httpSession;
-    
+	
     @GetMapping("/getjsessionid")
     public String getJessionID(HttpServletRequest request) {
     	httpSession = request.getSession();
@@ -27,4 +28,22 @@ public class SessionController {
     	return "JSESSIONID: " + httpSession.getId() + "<br>" + 
 		"每次訪問 '/invalidjsessionid' 取得 JSESSIONID 後，即刻失效! 因此再次訪問，則 JSESSIONID 皆異 (*請開 F12)";
     }
+    
+    @GetMapping("/getSession4SpecificPath")
+    public String getSession4SpecificPath(HttpServletRequest request, HttpServletResponse response) {    	
+    	UUID uuid = UUID.randomUUID();
+    	Cookie cookie = new Cookie("specific_session", uuid.toString());
+    	cookie.setMaxAge(5);
+    	cookie.setPath("/getSession4SpecificPath");
+    	cookie.setDomain("localhost");
+    	cookie.setHttpOnly(true);
+    	cookie.setSecure(true);
+    	int maxAge = cookie.getMaxAge();
+    	response.addCookie(cookie);
+    	return "specific_session: " + cookie.getValue() + "<br>" +  
+    		   "maxAge: " + maxAge + " seconds" + "<br>" + 
+    		   "特定 path 才有此 session 且有效期 5 秒";
+    }
+  
+    
 }
